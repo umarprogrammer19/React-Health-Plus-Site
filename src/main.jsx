91b0ee14ede1737c9afe7d99917dde85ddd62d02
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-18 20:35:39 +0500

    Developed Hero

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,32 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home.jsx';
 
 const router = createBrowserRouter([{
   path: "/",
   element: <App />,
-  // children: [{
-  //   path: "",
-  //   element: <Home />
-  // }, {
-  //   path: "Services",
-  //   element: <Services />
-  // }, {
-  //   path: "About",
-  //   element: <AboutPage />
-  // }, {
-  //   path: "Reviews",
-  //   element: <ReviewsPage />
-  // }, {
-  //   path: "Doctors",
-  //   element: <DoctorsPage />,
-  // }, {
-  //   path: "*",
-  //   element: <NotFound />
-  // }]
+  children: [{
+    path: "",
+    element: <Home />
+  }, // {
+    //   path: "Services",
+    //   element: <Services />
+    // }, {
+    //   path: "About",
+    //   element: <AboutPage />
+    // }, {
+    //   path: "Reviews",
+    //   element: <ReviewsPage />
+    // }, {
+    //   path: "Doctors",
+    //   element: <DoctorsPage />,
+    // }, {
+    //   path: "*",
+    //   element: <NotFound />
+    // }
+  ]
 }])
 
 createRoot(document.getElementById('root')).render(
