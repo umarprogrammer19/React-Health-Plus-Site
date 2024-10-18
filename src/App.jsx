90a38f17a69069087168d0e15c012cc55ad3c5bc
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-18 20:25:18 +0500

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Navbar />
       <Outlet />
     </>
   )
