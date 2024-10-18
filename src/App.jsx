9647c3e74a60aedc840fa897046c9c4123912851
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-18 21:48:57 +0500

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <>
+    <div className="home-section">
       <Navbar />
       <Outlet />
       <Footer />
-    </>
+    </div>
   )
 }
